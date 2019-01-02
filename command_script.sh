7cd1e0e7419d7d0f17502b0638723de806264b34
Hasan <44348022+138Ahmed@users.noreply.github.com>
2019-01-02 22:55:35 +0000

    v1.01

--- command_script.sh
+++ command_script.sh
@@ -1,21 +1,18 @@
+#!bin/bash
 sudo apt-get update
 echo 'sleeping for 60 seconds'
 sleep 60
 echo 'back online'
-
 sudo apt-get -y install python3-requests
 echo 'sleeping for 60 seconds'
 sleep 60
 echo 'back online'
-
 # git clone https://github.com/138Ahmed/google_cloud_API_script
 # echo 'sleeping for 45 seconds'
 # sleep 45
 # echo 'back online'
-
 python3 api_script.py 
 echo 'sleeping for 120 seconds'
 sleep 120
 echo 'back online'
-
 gsutil cp api_output.* gs://api_json_data/api_output/
